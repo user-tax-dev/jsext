7598bd530c695dfa5fc650ab9485a7c5deff5b46
usrtax <i@user.tax>
2022-10-21 13:55:04 +0800

    v0.0.4

--- dist.sh
+++ dist.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
-_DIR=$(cd "$(dirname "$0")"; pwd)
+_DIR=$(
+  cd "$(dirname "$0")"
+  pwd
+)
 
 cd $_DIR
 
 set -ex
 
-version=$(cat package.json|jq -r '.version')
+version=$(cat package.json | jq -r '.version')
 
 mdi
 
-add(){
-git add -u
-git commit -m v$version || true
+add() {
+  git add -u
+  git commit -m v$version || true
 }
-
-add
 
 git pull
 nr build
 npm set unsafe-perm true
+add
 npm version patch
 add
 git push
